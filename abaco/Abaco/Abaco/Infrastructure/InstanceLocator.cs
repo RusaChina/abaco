dd30db6a73dff40ceac9220b21d8b2c72fefa16b
RusaChina <paoli459@gmail.com>
2019-07-11 10:30:42 -0600

    LoginPage

---
+++ abaco/Abaco/Abaco/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,27 @@
+﻿using Abaco.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Abaco.Views;
+
+namespace Abaco.Infrastructure
+{
+    
+   public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ abaco/Abaco/Abaco/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abaco.ViewModels
+{
+   public class MainViewModel
+    {
+    }
+}
